55efdce2c0fd58213c5d41b4ecc7e194a6776991
hassanshamsi5 <shamsihassan5@gmail.com>
2023-08-22 22:27:06 +0500

    comit

--- src/components/Aboutus/about.jsx
+++ src/components/Aboutus/about.jsx
@@ -8,9 +8,9 @@ export default function about() {
             <div className="accordion" id="accordionExample">
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
-                        {/* <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                             Accordion Item #1
-                        </button> */}
+                        </button>
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
