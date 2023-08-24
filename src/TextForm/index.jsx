94cdadc2bd45af65d74477807722b5926e2ccce9
hassanshamsi5 <shamsihassan5@gmail.com>
2023-08-24 17:00:32 +0500

    commit

--- src/TextForm/index.jsx
+++ src/TextForm/index.jsx
@@ -45,7 +45,7 @@ function App(props) {
     return (
         <div>
             <div className="container">
-                <h1 className="display-6 fw-bold">{props.heading}</h1>
+                <h1>{props.heading}</h1>
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleChange} id="myBox" cols="30" rows="10"></textarea>
                 </div>
@@ -53,18 +53,16 @@ function App(props) {
                 <button className="btn btn-primary mx-1" onClick={handleDownClick}>LowerCase</button>
                 <button className="btn btn-primary mx-1" onClick={capitalizeText}>Capitalize Text</button>
                 <button className="btn btn-primary mx-1" onClick={clearText}>Clear Text</button>
-                <button className="btn btn-outline-warning mx-1" onClick={speak}>Speak</button>
-                <button className="btn btn-outline-warning mx-1"  onClick={handleCopy}>Copy Text</button>
-                <button className="btn btn-outline-warning mx-1"  onClick={handleExtraSpace}>Remove Extra Space</button>
-
+                <button className="btn btn-primary mx-1" onClick={speak}>Speak</button>
+                <button className="btn btn-primary mx-1"  onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-1"  onClick={handleExtraSpace}>Remove Extra Space</button>
             </div>
-            <div className="container">
-                <h1 className="fs-3 fw-bold">Your Text Summary</h1>
+            <div className="container my-3">
+                <h1>Your Text Summary</h1>
                 <p>{text.split(" ").length} words and  {text.length} characters </p>
                 <p>{0.008 * text.split(" ").length} <b> Minutes Read</b></p>
-                <h2 className="fs-3 fw-bold">Preview</h2>
+                <h2>Preview</h2>
                 <p>{text}</p>
-
             </div>
         </div>
     )
