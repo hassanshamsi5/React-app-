00db6b16a8d7205b062874952a4881cf5e2b6d67
hassanshamsi5 <shamsihassan5@gmail.com>
2023-08-25 23:45:11 +0500

    comit

--- src/TextForm/index.jsx
+++ src/TextForm/index.jsx
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
 
 function App(props) {
+    // let disabled=true
     const [text, setText] = useState(''); // Initialize with an empty string
 
     const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
-        props.showAlert("Converted To Uppercase","success")
+        props.showAlert("Converted To Uppercase", "success")
     }
     const handleDownClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
-        props.showAlert("Converted To LowerCase","success")
-
+        props.showAlert("Converted To LowerCase", "success")
     }
     const clearText = () => {
         let newText = ''
         setText(newText);
-        props.showAlert("TextClear","success")
-
+        props.showAlert("TextClear", "success")
         // window.location.reload(false);
     }
     const capitalizeText = () => {
@@ -27,28 +26,25 @@ function App(props) {
             .map(word => word.charAt(0).toLocaleUpperCase() + word.slice(1).toLowerCase())
             .join(" ")
         setText(newText)
-        props.showAlert("Converted To CapitalizeText","success")
-
+        props.showAlert("Converted To CapitalizeText", "success")
     }
     const speak = () => {
         let msg = new SpeechSynthesisUtterance();
         msg.text = text;
         window.speechSynthesis.speak(msg);
-        props.showAlert("Speak","success")
-
+        props.showAlert("Speak", "success")
     }
     const handleCopy = () => {
         var text = document.getElementById('myBox')
         text.select();
-        text.setSelectionRange(0,9999);
+        document.getSelection().removeAllRanges()
         navigator.clipboard.writeText(text.value);
-        props.showAlert("Copy all Text","success")
-
+        props.showAlert("Copy all Text", "success")
     }
-    const handleExtraSpace = ()=>{
+    const handleExtraSpace = () => {
         let newText = text.split(/[ ]+/)
         setText(newText.join(" "))
-        props.showAlert("Remove Extra Space","success")
+        props.showAlert("Remove Extra Space", "success")
     }
     const handleChange = (event) => {
         setText(event.target.value);
@@ -60,20 +56,20 @@ function App(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleChange} id="myBox" cols="30" rows="10"></textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={handleUpClick}>UpperCase</button>
-                <button className="btn btn-primary mx-1" onClick={handleDownClick}>LowerCase</button>
-                <button className="btn btn-primary mx-1" onClick={capitalizeText}>Capitalize Text</button>
-                <button className="btn btn-primary mx-1" onClick={clearText}>Clear Text</button>
-                <button className="btn btn-primary mx-1" onClick={speak}>Speak</button>
-                <button className="btn btn-primary mx-1"  onClick={handleCopy}>Copy Text</button>
-                <button className="btn btn-primary mx-1"  onClick={handleExtraSpace}>Remove Extra Space</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>UpperCase</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleDownClick}>LowerCase</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={capitalizeText}>Capitalize Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={clearText}>Clear Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={speak}>Speak</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpace}>Remove Extra Space</button>
             </div>
             <div className="container my-3">
                 <h1>Your Text Summary</h1>
-                <p>{text.split(" ").length} words and  {text.length} characters </p>
-                <p>{0.008 * text.split(" ").length} <b> Minutes Read</b></p>
+                <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length} words and  {text.length} characters </p>
+                <p>{0.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length} <b> Minutes Read</b></p>
                 <h2>Preview</h2>
-                <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+                <p>{text.length > 0 ? text : "Nothing to Preview!"}</p>
             </div>
         </div>
     )
