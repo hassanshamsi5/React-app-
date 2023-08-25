beccdb03ba21872a294e63125b88a8b4fe1a10b3
hassanshamsi5 <shamsihassan5@gmail.com>
2023-08-26 00:06:31 +0500

    comit

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -100,16 +100,16 @@ function Header(props) {
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/alert">
-
+                  
                 </Link>
               </li>
             </ul>
 
             <div className="d-flex">
-              <div className="bg-primary rounded mx-2" onClick={() => { props.toggleMode('primary') }} style={{ height: '30px', width: '30px', cursor: 'pointer' }}></div>
+              <div className="bg-primary rounded mx-2" onClick={() =>{props.toggleMode('primary')}} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
             </div>
-            <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
-              <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+            <div className={`form-check form-switch text-${props.mode === 'light'?'dark':'light'}`}>
+              <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
               <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Toggle Mode</label>
             </div>
           </div>
